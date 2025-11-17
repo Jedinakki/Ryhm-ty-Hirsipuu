f355ed690f3b76db08087150f46a282f80a0b9cf
Miigou <mvolas03@gmail.com>
2025-11-17 12:19:34 +0200

    Update sana-valinta.py

--- sana-valinta.py
+++ sana-valinta.py
@@ -1,21 +1,21 @@
 import random
 
-def valitse_sana():
+class SanaValinta:
+    def valitse_sana(self):
+        with open('sanat.txt', 'r') as tiedosto:
+            sanat = []
+            for rivi in tiedosto:
+                rivi = rivi.strip()
+                if rivi and not rivi.startswith('#'):
+                    sanat.append(rivi.upper())
+            return random.choice(sanat)
 
-    with open('sanat.txt', 'r') as tiedosto:
-        sanat = []
-        for rivi in tiedosto:
-            rivi = rivi.strip()
-            if rivi and not rivi.startswith('#'):
-                sanat.append(rivi.upper())
-        
-        return random.choice(sanat)
+    def nayta_alaviivat(self, sana):
 
-def nayta_alaviivat(sana):
-    """Näyttää sanan alaviivoineen _ _ _"""
-    return ' '.join('_' for _ in sana)
+        return ' '.join('_' for _ in sana)
 
 if __name__ == "__main__":
-    valittu_sana = valitse_sana()
+    sana_valinta = SanaValinta()
+    valittu_sana = sana_valinta.valitse_sana()
     print(f"Arvaa sana: ")
-    print(nayta_alaviivat(valittu_sana))
+    print(sana_valinta.nayta_alaviivat(valittu_sana))
