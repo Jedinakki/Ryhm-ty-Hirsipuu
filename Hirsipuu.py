f033a2b8d7fcfd0cbe265e0df8173f896ec393a8
Miigou <mvolas03@gmail.com>
2025-11-18 12:26:17 +0200

    Update Hirsipuu.py
    Updated game logic to reveal correct word on loss and show letters used.

--- Hirsipuu.py
+++ Hirsipuu.py
@@ -4,28 +4,35 @@ from Hirsipuun_piirto import *
 
 def peli():
     sana = SanaValinta('sanat.txt')
-    tarkista = Tarkistin(sana.muunna())
+    tarkista = Tarkistin(sana.valitse_sana())
     puu = Piirto()
 
     laskin = 1
 
     while True:
         print(puu)
+        tarkista.anna_kirjaimet()
         print(tarkista)
 
         if laskin == 12:
             print("Hävisit pelin!")
+            print(f"Oikea sana oli {sana.valittu_sana}")
             break
 
-        if sana == tarkista:
+        if sana.valittu_sana == tarkista.paljastus:
             print("Voitit pelin!")
             break
+
+
 
         merkki = input("Anna kirjain: ")
 
         if tarkista.tarkista(merkki) == False:
             laskin += 1
             puu.seuraava_kuva()
+
+        
+
 
 
 if __name__ == "__main__":
@@ -35,5 +42,6 @@ if __name__ == "__main__":
 
 
         
+
 
 
